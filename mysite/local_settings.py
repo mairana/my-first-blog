a35adf71bfe100cc83843d718287eaf3bab8bcf9
Johnny Ivanovich <johnnyivanovich@gmail.com>
2019-11-26 19:47:21 -0500

    Re committing files to GitHub.

--- mysite/local_settings.py
+++ mysite/local_settings.py
@@ -76,10 +76,15 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'djangogirls',
+        'USER': 'johnnyivanovich',
+        'PASSWORD': 'EP84ji+_',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
